5a70bf350d743c2c84cb1f56a852e1587418bc8b
Jmz <jmztaylor@gmail.com>
2014-03-09 08:28:00 +0100

    Allow disabling of FC dialogs [1/2]
    Change-Id: If87aa74ebb45849f6e5fe3a53ce9beeaac28f624
    Signed-off-by: NoelMacwan <yulespencer@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	services/core/java/com/android/server/am/ActivityManagerService.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3721,6 +3721,12 @@ public final class Settings {
         public static final String STATUSBAR_COMPONENT = "statusbar_component";
 
         /**
+         * Disable FC Notifications
+         * @hide
+         */
+        public static final String DISABLE_FC_NOTIFICATIONS = "disable_fc_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
